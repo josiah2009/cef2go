6c2939b369e91b5cb7ffb0f7f31d09b76c32db5c
Aaron Quint <aaron@quirkey.com>
2014-10-06 16:24:59 -0400

    Wrap cef types

--- cef/render_handler.go
+++ cef/render_handler.go
@@ -12,22 +12,35 @@ import (
 	"unsafe"
 )
 
+type CefRect C.cef_rect_t
+type CefScreenInfo C.cef_screen_info_t
+type CefPaintElementType C.cef_paint_element_type_t
+type CefCursorHandle C.cef_cursor_handle_t
+
+func (r *CefRect) SetDimensions(x, y, width, height int) {
+	rect := (*C.cef_rect_t)(r)
+	rect.x = (C.int)(x)
+	rect.y = (C.int)(y)
+	rect.width = (C.int)(width)
+	rect.height = (C.int)(height)
+}
+
 type RenderHandler interface {
-	GetRootScreenRect(*C.cef_rect_t) int
-	GetViewRect(*C.cef_rect_t) int
+	GetRootScreenRect(*CefRect) int
+	GetViewRect(*CefRect) int
 	GetScreenPoint(int, int, *int, *int) int
-	GetScreenInfo(*C.cef_screen_info_t) int
+	GetScreenInfo(*CefScreenInfo) int
 	OnPopupShow(int)
-	OnPopupSize(*C.cef_rect_t)
-	OnPaint(C.cef_paint_element_type_t, C.size_t, unsafe.Pointer, unsafe.Pointer, int, int)
-	OnCursorChange(C.cef_cursor_handle_t)
+	OnPopupSize(*CefRect)
+	OnPaint(CefPaintElementType, int, unsafe.Pointer, unsafe.Pointer, int, int)
+	OnCursorChange(CefCursorHandle)
 	OnScrollOffsetChanged()
 }
 
 //export go_RenderHandlerGetRootScreenRect
 func go_RenderHandlerGetRootScreenRect(browserId int, rect *C.cef_rect_t) int {
 	if b, ok := BrowserById(browserId); ok {
-		return b.RenderHandler.GetRootScreenRect(rect)
+		return b.RenderHandler.GetRootScreenRect((*CefRect)(rect))
 	}
 	return 0
 }
@@ -35,7 +48,7 @@ func go_RenderHandlerGetRootScreenRect(browserId int, rect *C.cef_rect_t) int {
 //export go_RenderHandlerGetViewRect
 func go_RenderHandlerGetViewRect(browserId int, rect *C.cef_rect_t) int {
 	if b, ok := BrowserById(browserId); ok {
-		return b.RenderHandler.GetViewRect(rect)
+		return b.RenderHandler.GetViewRect((*CefRect)(rect))
 	}
 	return 0
 }
@@ -51,7 +64,7 @@ func go_RenderHandlerGetScreenPoint(browserId, x, y int, screenX *int, screenY *
 //export go_RenderHandlerGetScreenInfo
 func go_RenderHandlerGetScreenInfo(browserId int, info *C.cef_screen_info_t) int {
 	if b, ok := BrowserById(browserId); ok {
-		return b.RenderHandler.GetScreenInfo(info)
+		return b.RenderHandler.GetScreenInfo((*CefScreenInfo)(info))
 	}
 	return 0
 }
@@ -66,37 +79,22 @@ func go_RenderHandlerOnPopupShow(browserId int, show int) {
 //export go_RenderHandlerOnPopupSize
 func go_RenderHandlerOnPopupSize(browserId int, size *C.cef_rect_t) {
 	if b, ok := BrowserById(browserId); ok {
-		b.RenderHandler.OnPopupSize(size)
+		b.RenderHandler.OnPopupSize((*CefRect)(size))
 	}
 }
 
 //export go_RenderHandlerOnPaint
 func go_RenderHandlerOnPaint(browserId int, paintType C.cef_paint_element_type_t, dirtyRectsCount C.size_t, dirtyRects unsafe.Pointer, buffer unsafe.Pointer, width, height int) {
 	if b, ok := BrowserById(browserId); ok {
-		b.RenderHandler.OnPaint(paintType, dirtyRectsCount, dirtyRects, buffer, width, height)
+		b.RenderHandler.OnPaint((CefPaintElementType)(paintType), int(dirtyRectsCount), dirtyRects, buffer, width, height)
 	}
-	// gbuffer := C.GoBytes(buffer, C.int(width*height*4))
-	// log.Debug("on paint %d %d %dx%d", browserId, len(gbuffer), width, height)
-	// bgra_image := &bgra.BGRA{Pix: gbuffer, Stride: 4 * width, Rect: image.Rect(0, 0, 700, 700)}
-	// filename := fmt.Sprintf("./buffer_%d.png", browserId)
-	// f, err := os.Create(filename)
-	// if err != nil {
-	// 	log.Error("Could not open file for writing %s", err)
-	// 	return
-	// }
-	// err = png.Encode(f, bgra_image)
-	// if err != nil {
-	// 	log.Error("Could not encode png %s", err)
-	// 	return
-	// }
-	// log.Debug("Wrote buffer to %s", filename)
 
 }
 
 //export go_RenderHandlerOnCursorChange
 func go_RenderHandlerOnCursorChange(browserId int, cursor C.cef_cursor_handle_t) {
 	if b, ok := BrowserById(browserId); ok {
-		b.RenderHandler.OnCursorChange(cursor)
+		b.RenderHandler.OnCursorChange((CefCursorHandle)(cursor))
 	}
 }
 
@@ -111,11 +109,11 @@ type DefaultRenderHandler struct {
 	Browser *Browser
 }
 
-func (d *DefaultRenderHandler) GetRootScreenRect(rect *C.cef_rect_t) int {
+func (d *DefaultRenderHandler) GetRootScreenRect(rect *CefRect) int {
 	return 0
 }
 
-func (d *DefaultRenderHandler) GetViewRect(rect *C.cef_rect_t) int {
+func (d *DefaultRenderHandler) GetViewRect(rect *CefRect) int {
 	return 0
 }
 
@@ -123,20 +121,20 @@ func (d *DefaultRenderHandler) GetScreenPoint(x, y int, screenX, screenY *int) i
 	return 0
 }
 
-func (d *DefaultRenderHandler) GetScreenInfo(info *C.cef_screen_info_t) int {
+func (d *DefaultRenderHandler) GetScreenInfo(info *CefScreenInfo) int {
 	return 0
 }
 
 func (d *DefaultRenderHandler) OnPopupShow(show int) {
 }
 
-func (d *DefaultRenderHandler) OnPopupSize(size *C.cef_rect_t) {
+func (d *DefaultRenderHandler) OnPopupSize(size *CefRect) {
 }
 
-func (d *DefaultRenderHandler) OnPaint(paintType C.cef_paint_element_type_t, dirtyRectsCount C.size_t, dirtyRects unsafe.Pointer, buffer unsafe.Pointer, width, height int) {
+func (d *DefaultRenderHandler) OnPaint(paintType CefPaintElementType, dirtyRectsCount int, dirtyRects unsafe.Pointer, buffer unsafe.Pointer, width, height int) {
 }
 
-func (d *DefaultRenderHandler) OnCursorChange(cursor C.cef_cursor_handle_t) {
+func (d *DefaultRenderHandler) OnCursorChange(cursor CefCursorHandle) {
 }
 
 func (d *DefaultRenderHandler) OnScrollOffsetChanged() {
